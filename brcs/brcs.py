d461cd0780303f63bdbdbdaf5b3b55c0a0b96dde
Yushi Yang <yangyushi1992@icloud.com>
2019-10-25 11:42:48 +0100

    update

--- brcs/brcs.py
+++ brcs/brcs.py
@@ -2,6 +2,9 @@
 import numpy as np
 
 def get(number, bias=(1.0, 0.7, 0.8), lightness=(0.25, 1.0)):
-    rgbs = np.random.uniform(lightness[0], lightness[1], (number, 3))
+    if number > 1:
+        rgbs = np.random.uniform(lightness[0], lightness[1], (number, 3))
+    else:
+        rgbs = np.random.uniform(lightness[0], lightness[1], 3)
     colors = rgbs * np.array(bias)
     return colors
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="brcs",
-    version="0.1.1",
+    version="0.1.2",
     author="Yushi Yang",
     description="Biased random colors",
     long_description=long_description,
